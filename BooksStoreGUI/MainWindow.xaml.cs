5e7c535cb43e8feb03335bf2fafaa9982192bc6b
rachelimish <157749744+rachelimish@users.noreply.github.com>
2024-03-21 21:38:53 +0200

    Update MainWindow.xaml.cs
    תיקון

--- BooksStoreGUI/MainWindow.xaml.cs
+++ BooksStoreGUI/MainWindow.xaml.cs
@@ -40,7 +40,7 @@ namespace BooksStoreGUI
             string Country = e1.Text;
             string Phone = f.Text;
             booksStoreBL.AddNewBook(id, FName, LName, City, Country, Phone);
-           // Applylist();
+           Applylist();
 
         }
     }
